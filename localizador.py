582b14b357c7a05b649c9dea273fb728d0ff1427
Bruno R. Toffoli <btoffoli@gmail.com>
2016-08-29 00:31:45 -0300

    more methods

--- localizador.py
+++ localizador.py
@@ -1,21 +1,51 @@
+import datetime
+
 from config import dbConfig
 from db.localizador_db import Dispositivo
-from models import Item, Leitura, Instalacao
+from models import Item, Leitura, Instalacao, TipoDispositivo
+from datetime import datetime
+
 
 class LocalizadorService:
 
     # TODO metodo inserirDispositivo, mapear tipo_dispositivo
-    # TODO mapear dispositvo e item em instalacao
+    # TODO mapear dispositvo e item em instalacao implementar teste p/ verificar
     #
-    def inserirDispositivo(self, tipo, codigo, nome, **kwargs):
-        return
+
+    @property
+    def __sessao(self):
+        dbConfig.session()
+
+    def inserirDispositivo(self, tipo, codigo, nome, commit=True):
+        disp = self.obterDispositivoPorCodigo()
+        if not disp:
+            disp = Dispositivo(tipo=tipo, nome=nome, codigo=codigo)
+            self.__sessao.add(disp)
+            if commit:
+                self.__sessao.commit()
+        return disp
+
+    def inserirTipoDispositivo(self, codigo, nome, commit=True):
+        tpDisp = self.obterTipoDispositivoPorCodigo()
+        if not tpDisp:
+            tpDisp = TipoDispositivo(nome=nome, codigo=codigo)
+            self.__sessao.add(tpDisp)
+            if commit:
+                self.__sessao.commit()
+        return tpDisp
+
+    def instalarDispositivoNoItem(self, item, dispositivo, data_instalacao=datetime.now(), commit=True):
+        inst = Instalacao(item=item, dispositivo=dispositivo, data_instalacao=data_instalacao)
+        sessao = self.__sessao
+        sessao.add(inst)
+        if commit:
+            sessao.commit()
+
+        return inst
 
 
 
-    def inserirItem(self, **kwargs):
-        nome = kwargs.get('nome')
-        codigo = kwargs.get('codigo')
-        nomeDispositivo = kwargs.get('nomeDispositivo')
+    def inserirItem(self, nome, codigo, commit=True):
         sessao = dbConfig.session()
 
         it = self.obterItemPorCodigo(codigo)
@@ -23,9 +53,17 @@ class LocalizadorService:
             it = Item(nome=nome, codigo=codigo)
             sessao.add(it)
         #Verificar existencia do item, codigo e instalacao
-        sessao.commit()
+        if commit:
+            sessao.commit()
         return it
 
+    def obterInstalacaoEmAbertoPorItemEDispositivo(self, item, dispositivo):
+        instalacao = None
+        q = dbConfig.session().query(Instalacao).filter(Item == item, )
+        itens = q.all()
+        if itens:
+            item = itens[0]
+        return item
 
     def obterItemPorNome(self, nome):
         item = None
@@ -51,16 +89,35 @@ class LocalizadorService:
             dispositivo = dispositivos[0]
         return dispositivo
 
-    def inserirLeitura(self, identificacao, dataHora, longitude, latitude):
+    def obterDispositivoPorCodigo(self, codigo):
+        dispositivo = None
+        q = dbConfig.session().query(Dispositivo).filter(Dispositivo.codigo == codigo)
+        dispositivos = q.all()
+        if dispositivos:
+            dispositivo = dispositivos[0]
+        return dispositivo
+
+    def inserirLeitura(self, identificacao, dataHora, longitude, latitude, commit=True):
         item = self.obterItemPorCodigo(identificacao)
         if item:
             leitura = Leitura(horario_leitura=dataHora, localizacao="POINT(%f %f)" %(longitude, latitude))
             sessao = dbConfig.session()
             sessao.add(leitura)
-            sessao.commit()
+            if commit:
+                sessao.commit()
             return leitura
         return
 
+    def obterTipoDispositivoPorCodigo(self, codigo):
+        tpDisp = None
+        q = dbConfig.session().query(Dispositivo).filter(Dispositivo.codigo == codigo)
+        tipos = q.all()
+        if tipos:
+            tpDisp = tipos[0]
+        return tpDisp
+
+    def inserirItemCompleto(self, nomeItem, codigoItem, nome):
+        pass
 
 
 
